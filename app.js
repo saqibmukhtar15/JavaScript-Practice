ffb67c2256aab0fb5fa4a1443331f71bda9aa17a
Saqib Mukhtar <saqibmukhtar15@gmail.com>
2026-04-24 17:12:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+// Ek file app.js create karo aur usmein "Hello World" print karo.
+console.log("Hello World!");
+
+// console.log() ka use karke apna naam print karo.
+console.log("Saqib Mukhtar")
+
+
+// alag lines mein apna naam aur age print karo.
+console.log("Naam: Saqib Mukhtar");
+console.log("Age: 31");
+
+// Ek variable bana kar usmein apna city store karo aur print karo.
+let city = "Lahore";
+console.log(city);
+
+// Ek hi file mein 3 different console.log() statements likho.
+console.log("Yeh pehla log hai");
+console.log("Yeh doosra log hai");
+console.log("Yeh teesra log hai");
+
+
+// let se ek variable banao aur usmein number store karo.
+let a = 15;
+console.log(a);
+
+// const se apna naam store karo.
+const Nickname = "Ali";
+console.log(Nickname);
+
+// var se ek variable banao aur usmein age store karo.
+var age = 31;
+console.log(age);
+
+// Ek variable bana kar uski value print karo.
+let num = 15;
+console.log(num);
+
+// let se variable bana kar uski value change karo (update karo).
+let o = 20;
+o = 25;
+console.log(o);
+
+
+// Ek string variable banao (apna naam).
+let name = "Saqib Mukhtar";
+console.log(name);
+
+// Ek number variable banao (age ya marks).
+let marks = 80;
+console.log(marks);
+
+// Ek boolean variable banao (true ya false).
+let isStudent = true;
+console.log(isStudent);
+
+// Ek variable ko null assign karo.
+let data = null;
+console.log(data);
+
+// Ek variable banao aur usay koi value assign na karo (undefined).
+let pen ;
+console.log(pen);
+
+
+// Ek string variable banao aur uska typeof check karo.
+let gender = "Male";
+console.log(typeof gender);
+
+// Ek number variable ka typeof check karo.
+let num1 = 10;
+console.log(typeof num);
+
+// Boolean ka typeof print karo.
+let isBoy = true;
+console.log(typeof isBoy);
+
+// Ek undefined variable ka typeof check karo.
+let value;
+console.log(typeof value);
+
+// Ek array ka typeof check karo (observe output).
+let arr = [1, 2, 3];
+console.log(typeof arr);
+
+
+// 2 numbers add karo aur result print karo
+let b = 10, c = 5;
+console.log(b + c);
+
+// 2 numbers subtract karo.
+let d = 10, e = 5;
+console.log(d - e);
+
+// 2 numbers multiply karo.
+let f = 10, g = 5;
+console.log(f * g);
+
+// 2 numbers divide karo.
+let h = 10, i = 5;
+console.log(h / i);
+
+// % operator use karke remainder find karo.
+let j = 10, k = 3;
+console.log(j % k);
+
+
+// Check karo 5 == 5 (result print karo).
+console.log(5 == 5);
+
+// Check karo 5 === "5"
+console.log(5 === "5");
+
+// Check karo 10 > 5
+console.log(10 > 5);
+
+// Check karo 3 < 1
+console.log(3 < 1);
+
+
+// Ek variable x = 5 banao aur x++ use karo.
+let x = 5;
+x++;
+console.log(x);
+
+// Ek variable y = 5 banao aur ++y use karo.
+let y = 5;
+++y;
+console.log(y);
+
+// Ek variable ko -- se decrease karo.
+let z = 5;
+z--;
+console.log(z);
+
+// Pre-increment ka result print karo.
+let l = 5;
+console.log(++l);
+
+// Post-increment ka result print karo (difference observe karo).
+let m = 5;
+console.log(m++);
+
+
